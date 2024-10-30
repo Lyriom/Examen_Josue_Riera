9a5c2f10277de738480ba7ab977def2c40c8a223
Josue Riera <josueriera23@hotmail.com>
2024-10-30 09:06:31 -0500

    Se hacen los modelos

--- Models/Celular.cs
+++ Models/Celular.cs
@@ -1,7 +1,21 @@
-﻿namespace Examen_Josue_Riera.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Examen_Josue_Riera.Models
 {
     public class Celular
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Modelo { get; set; }
+
+        [Range(2000, 2024)]
+        public int Año { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Precio { get; set; }
 
     }
 }
---
+++ Models/JRIera.cs
@@ -0,0 +1,31 @@
+﻿using Examen_Josue_Riera.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class JRiera
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Nombre { get; set; }
+
+    [Range(18, 100)]
+    public int Edad { get; set; }
+
+    [DataType(DataType.Currency)]
+    public decimal Salario { get; set; }
+
+    [Required]
+    public bool Activo { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateTime FechaRegistro { get; set; }
+
+    public Celular? Celular { get; set; }
+
+    [ForeignKey(nameof(Celular))]
+    public int IdCelular { get; set; }
+
+}
